c831b5a8e1836b463acc6e20db8bf03e20396b5f
RenzoTorrente <renzotor7@hotmail.com>
2021-09-10 18:10:48 -0300

    performing simple validation

--- dataview.php
+++ dataview.php
@@ -4,10 +4,18 @@ $dni = $_POST['dni'];
 $email = $_POST['email'];
 $carrera = $_POST['carrera'];
 
-echo "<h3>Se ha inscripto con exito verifique sus datos:</h3>";
-echo "nombre de alumno : " . $nombre . "<br/>";
-echo "dni : " . $dni . "<br/>";
-echo "email de alumno: " . $email . "<br/>";
-echo "escogió la carrera de : " . $carrera . "<br/>";
+if(empty($carrera) or empty($dni) or empty($carrera) or empty($email)){
+
+    echo("<h2>No pudimos registrarte, algunos campos del formulario se encontraban vacios</h2>")
+
+}else{
+    echo "<h3>Se ha inscripto con exito verifique sus datos:</h3>";
+    echo "nombre de alumno : " . $nombre . "<br/>";
+    echo "dni : " . $dni . "<br/>";
+    echo "email de alumno: " . $email . "<br/>";
+    echo "escogió la carrera de : " . $carrera . "<br/>";
+
+}
+
  
 ?>
